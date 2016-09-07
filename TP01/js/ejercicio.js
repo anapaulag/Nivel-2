1a4f20f54527909e4b594fbd32cd9735bf3e0e91
anapaulag <anapaula_g85@hotmail.com>
2016-09-07 14:45:58 -0400

    Tp01

---
+++ TP01/js/ejercicio.js
@@ -0,0 +1,9 @@
+var calle = prompt ('ingrese el numero de su calle');
+calle = parseInt (calle);
+var piso = parseInt (prompt ('ingrese el piso donde vive'));
+alert (calle + piso);
+alert (calle - piso);
+alert (calle * piso);
+alert (calle / piso);
+
+var numeroC = parseFloat (prompt('Ingrese decimal'))
